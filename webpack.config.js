cacef85aeefc21d6e2cca5963d521a50dcf25ef1
zjlin <zijunlin32@gmail.com>
2021-08-11 18:06:48 -1000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,30 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   module: {
-    rules
-  }
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { preset: ["@babel/env"] }
+      },
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentbase: path.join(__dirname, 'public/'),
+    port: 3000,
+    publicPath: 'http://localhost:3000/dist/',
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 }
