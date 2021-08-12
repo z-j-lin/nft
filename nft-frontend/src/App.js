1312b517510013faa5eecf1025a4d34a861fba43
zjlin <zijunlin32@gmail.com>
2021-08-12 13:19:56 -1000

    loading web3 into app.js works

--- nft-frontend/src/App.js
+++ nft-frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Web3 from 'web3'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import NavBar from './NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  async loadWeb3() {
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('no ethereum browser detect, try installing metamask')
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar/>
+          <div id = "page-body">
+            {/*homepage */}
+            <Route path = "/hello" component={HomePage} exact/>
+          </div>
+        </div>
+      </Router>
+      
+    );
+  }
 }
 
 export default App;
---
+++ nft-frontend/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    <h1>hello</h1>
+  </>
+
+);
+export default HomePage;
