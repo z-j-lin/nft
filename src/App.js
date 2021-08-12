31c200ace12ef6f84ae0308410fcdcb099c35d30
zjlin <zijunlin32@gmail.com>
2021-08-11 17:44:47 -1000

    Added webpacks

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1>life sucks get over it</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules
+  }
+}
