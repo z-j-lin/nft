27f3d4d939901ae15e63d373ec85090740a67d9b
zjlin <zijunlin32@gmail.com>
2021-08-15 17:05:16 -1000

    changed login functionality, included login button handler

--- nft-frontend/src/App.js
+++ nft-frontend/src/App.js
@@ -10,9 +10,37 @@ import NavBar from './NavBar';
 //import detectEthereumProvier from "@metamask/detect-provider"
 
 class App extends Component {
-
+  
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      account: ""
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('no ethereum browser detect, try installing metamask')
+    }
+  }
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    //load account
+    const accounts = await web3.eth.getAccounts()
+    //this is the first account in the wallet
+    this.setState({account: accounts[0] })
   }
 
   render() {
@@ -22,8 +50,7 @@ class App extends Component {
           <NavBar account = {this.state.account}/>
           <div id = "page-body">
             {/*homepage */}
-            {/*<Router path ="/" component={}/>*/}
-            <Route path = "/login" component={Login} exact/>
+            <Route path = "/Login" component={Login} exact/>
           </div>
         </div>
       </Router>
--- nft-frontend/src/NavBar.js
+++ nft-frontend/src/NavBar.js
@@ -1,15 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {menu} from 'semantic-ui-react';
 
+class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLoginClick = this.handleLogoutClick.bind(this);
+    this.state = {
+      isLoggedIn: false
+    };
 
-const NavBar = (props) => (
-  <nav>
-    <ul>
-      <li><Link to = "/">Home</Link></li>
-      <li><Link to="/hello">hello</Link></li>
-      <li><span id = "account">{props.account}</span> </li>
-    </ul>
-  </nav>
+  }
+  handleLoginClick(){
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick(){
+    this.setState({isLoggedIn:false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    /*
+    let loginButton;
+    if(isLoggedIn){
+      mainButton = <loginButton onClick={this.handleLogoutClick}/>
+    } else{
+      mainButton = <logoutButton onClick={this.handleLogoutClick}/>
+    }*/
+    return(
+      <div>
+        <div class="ui menu">
+          <div class="header item">
+            Our Company
+          </div>
+          <a class="item">
+            About Us
+          </a>
+          <a class="item">
+            Jobs
+          </a>
+          <a class="item">
+            Login
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+ReactDOM.render(
+  <NavBar />,
+  document.getElementById('root')
 );
-
+ 
 export default NavBar; 
