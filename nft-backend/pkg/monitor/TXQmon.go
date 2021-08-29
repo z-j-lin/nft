79696efcd2f416a1b0baf562828f6a56bf722590
z-j-lin <zijunlin32@gmail.com>
2021-08-29 13:51:21 -1000

    update main

--- nft-backend/pkg/monitor/TXQmon.go
+++ nft-backend/pkg/monitor/TXQmon.go
@@ -1,27 +1,71 @@
 package monitor
 
+import (
+	"github.com/z-j-lin/nft/tree/main/nft-backend/pkg/blockchain"
+)
+
 type txqmon struct {
-	db Database
+	db    Database
+	MintQ chan *blockchain.transaction
 }
 
-func NewTXQmon(rdb Database) *txqmon {
+func NewQmon(rdb Database) *txqmon {
+	//channel for storing mint jobs
+	//takes a pointer to a transaction object
 	Mintque := make(chan *blockchain.transaction, 3)
-	return &txqmon{
-		db: rdb,
+	Qmon := &txqmon{
+		db:    rdb,
+		MintQ: Mintque,
 	}
+	return Qmon
 }
 
 //function to start the transaction que monitoring loop
 func (qmon *txqmon) startTXQmonLoop() {
-	go qmon.loop()
+	go qmon.TXloop()
 }
-func (qmon *txqmon) loop() {
+
+//constantly checks the loops
+//start jobs by loading into the job queue channel
+//
+func (qmon *txqmon) TXloop() {
 	for {
 		//check if the transaction queue has a job
 		account, resourceID := qmon.db.DQmint()
 		if account != "" {
 			//initiate a new transaction
 			//load tx into buffered channel
+			blockchain.NewTransaction()
+		}
+
+	}
+}
+
+//loops through storing items into a verfication channel
+//the channel should take a unique verfication object for each transaction
+func (qmon *txqmon) Verfificationloop() {
+	for {
+		//check if the transaction queue has a job
+		hash, account, resourceID := qmon.db.DQpending()
+		if account != "" {
+			//initiate a new transaction
+			//load tx into buffered channel
+			blockchain.NewTransaction()
+		}
+	}
+}
+
+func (qmon *txqmon) txqmanager() {
+	//when there are no jobs in the que, kill the manager
+	for {
+		select {
+
+		case done <- killChan:
+			return
+		//start a transaction
+		case tx <- Minq:
+
+			//start a mint worker
 		}
 
 	}
